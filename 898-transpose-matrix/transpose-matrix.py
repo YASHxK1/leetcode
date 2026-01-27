2a0c3e4f945c08907fce394115a00f069d6c711b
yash <132455639+YASHxK1@users.noreply.github.com>
2026-01-28 01:05:30 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (7.23%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        matrix = np.array(matrix)
+        transp = matrix.T.tolist()
+
+        return transp
+        
